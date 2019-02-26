6a63c0865d79edf097bb517068436d7d490cb5bd
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-02-26 02:32:13 +0200

    Updates

---
+++ precache-manifest.8adbf49b43b03482ea7bd264d41df1ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bceadcfd361d55c15a8aacc0e33ec05",
+    "url": "/random-quote-generator/static/media/b-9.4bceadcf.jpg"
+  },
+  {
+    "revision": "b3249faac51bc1f99340",
+    "url": "/random-quote-generator/static/js/runtime~main.b3249faa.js"
+  },
+  {
+    "revision": "9fecc2cc2a2c72a5381f",
+    "url": "/random-quote-generator/static/js/main.9fecc2cc.chunk.js"
+  },
+  {
+    "revision": "610d4d736b3be0ffa159",
+    "url": "/random-quote-generator/static/js/2.610d4d73.chunk.js"
+  },
+  {
+    "revision": "9fecc2cc2a2c72a5381f",
+    "url": "/random-quote-generator/static/css/main.1c3b5136.chunk.css"
+  },
+  {
+    "revision": "7e043ee2eff7444b521d7de037fa02a2",
+    "url": "/random-quote-generator/index.html"
+  }
+];
